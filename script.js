49a47f03ae0d3176dee77abeb2a62c68ac563106
Diego Bonilla <dcbcueva@gmail.com>
2022-09-26 11:04:47 -0500

    feat: creaqcion de script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+console.log("saoko papi saoko");
+document.getElementById("generation").style.color="grey";
+function colorTitulo(){
+    console.log("Pulso el botón de generation");
+    document.getElementById("generation").style.color ="blue";
+}
